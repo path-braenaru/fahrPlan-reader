99cede5a24765a7ecece824b77793f57978fde3a
path-braenaru <path_braenaru@inbox.ru>
2017-12-18 00:41:48 +0000

    Initial for 34C3 version

--- README.md
+++ README.md
@@ -5,22 +5,19 @@ fahr.py is an offline Fahrplan checking script written in Python 3.  Using the f
 schedule JSON, the script will either search for a talk by string or simply show remaining 
 talks for the day.  Don't let network outages ruin schedule checks!
 
-Usage: python fahr.py [-t $string] [-n]
+Usage: python fahr.py [-t/--talk $string] [-n] [--track $string]
 
--t $string will search all titles for $string and return talk details
--n will show remaining talks for the current date
++ -t $string will search all titles for $string and return talk details  
++ -n will show remaining talks for the current date  
++ --track will show all results for a given track  
 
-talks that have already passed are returned in red, talks yet to occur wil lreturn in green
-
-fahr_py2.py is included for Python 2
+talks that have already passed are returned in red, talks yet to occur will return in green
 
 Deps
 ----
 
-argparse
-
-json
-
-time
++argparse  
++json  
++time  
 
 The fahrplan schedule.json file is included, but please check for CCC updates to this file
--- fahr.py
+++ fahr.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 
 
 import json
@@ -8,91 +8,106 @@ from time import localtime, strftime, strptime
 
 
 class co:
-        GR = '\033[92m'
-        RE = '\033[91m'
-        BO = '\033[1m'
-        E = '\033[0m'
+	GR = '\033[92m'
+	RE = '\033[91m'
+	BO = '\033[1m'
+	E = '\033[0m'
 
 
-class factory():
+class process():
 
-    def load(self):
-        with open('schedule.json', 'r') as jsonfile:
-            for entry in jsonfile.readlines():
-                data = json.loads(entry)
-            jsonfile.close()
-        return data
+	def load(self):
+		with open('schedule.json', 'r') as jsonfile:
+			for entry in jsonfile.readlines():
+				data = json.loads(entry)
+			jsonfile.close()
+		return data
 
-    def extract(self, data):
-        rooms = [
-            'Saal 1',
-            'Saal 2',
-            'Saal 6',
-            'Saal G'
-        ]
-        procData = []
-        for x in data['schedule']['conference']['days']:
-            for room in rooms:
-                for i in x['rooms'][room]:
-                    tup = (i['title'], i['room'], i['date'], i['start'], i['duration'])
-                    if tup not in procData:
-                        procData.append(tup)
-        return procData
+	def extract(self, data):
+		rooms = [
+			'Saal Clarke',
+			'Saal Adams',
+			'Saal Borg',
+			'Saal Dijkstra']
+		procData = []
+		for x in data['schedule']['conference']['days']:
+			for room in rooms:
+				for i in x['rooms'][room]:
+					tup = (i['title'], i['room'], i['date'],
+						i['start'], i['duration'], i['track'])
+					if tup not in procData:
+						procData.append(tup)
+		return procData
+
+
+	def resultFormat(self, talk_dt, dt, x):
+		(talk, room, date, start, duration, track) = x
+		if talk_dt > dt:
+			a = co.GR
+		else:
+			a = co.RE
+		match = '%s%s\n - Room: %s\n - Date: %s\n - ' % (
+				a, talk, room, date[0:10]) +\
+			'Track: %s\n - Start: %s\n - Duration: %s%s\n' % (
+				track, start, duration, co.E)
+		return match
 
 
 def __init__():
-    p = argparse.ArgumentParser()
-    p.add_argument(
-        '-t', '--title'
-    )
-    p.add_argument(
-        '-n', '--next', action='store_true'
-    )
-    args = p.parse_args()
-    title = args.title
-    next = args.next
-    f = factory()
-    data = f.load()
-    dt = localtime()
-    d = strftime('%Y-%m-%d', localtime())
-    d_struct = strptime(d, '%Y-%m-%d')
-    t = strftime('%H:%M', localtime())
-    t_struct = strptime(t, '%H:%M')
-    results = []
-    procData = f.extract(data)
-    for x in procData:
-        (talk, room, date, start, duration) = x
-        talk_dt = date[0:10] + ' ' + start
-        talk_dt = strptime(talk_dt, '%Y-%m-%d %H:%M')
-        talk_d = strptime(date[0:10], '%Y-%m-%d')
-        talk_t = strptime(start, '%H:%M')
-        if title:
-            if title.lower() in talk.lower():
-                if talk_dt > dt:
-                    a = co.GR
-                #elif date == d:
-                #    if start > t:
-                #        a = co.GR
-                #    else:
-                #        a = co.RE
-                else:
-                    a = co.RE
-                match = '%s%s\n - Room: %s\n - Date: %s\n - Start: %s\n - Duration: %s%s\n' % (
-                        a, talk, room, date[0:10], start, duration, co.E)
-                if match not in results:
-                    results.append(match)
-        elif next:
-            if talk_d == d_struct:
-                if talk_t > t_struct:
-                    match = '%s%s\n%s - %s%s\n' % (co.BO, talk, start, room, co.E)
-                    results.append(match)
-        else:
-            print('Use -t $string to search for a talk')
-            print('Use -n|--next to see remaining talks for today')
-            exit(0)
-    for entry in results:
-        print(entry)
-    print('%sCurrent Time: %s%s%s' % (co.BO, co.GR, t, co.E))
+	p = argparse.ArgumentParser()
+	p.add_argument(
+		'-t', '--title'
+	)
+	p.add_argument(
+		'-n', '--next', action='store_true'
+	)
+	p.add_argument(
+		'--track')
+	args = p.parse_args()
+	title = args.title
+	next = args.next
+	trackSearch = args.track
+	f = process()
+	data = f.load()
+	dt = localtime()
+	d = strftime('%Y-%m-%d', localtime())
+	d_struct = strptime(d, '%Y-%m-%d')
+	t = strftime('%H:%M', localtime())
+	t_struct = strptime(t, '%H:%M')
+	results = []
+	procData = f.extract(data)
+	for x in procData:
+		(talk, room, date, start, duration, track) = x
+		talk_dt = date[0:10] + ' ' + start
+		talk_dt = strptime(talk_dt, '%Y-%m-%d %H:%M')
+		talk_d = strptime(date[0:10], '%Y-%m-%d')
+		talk_t = strptime(start, '%H:%M')
+		if title:
+			if title.lower() in talk.lower():
+				match = f.resultFormat(talk_dt, dt, x)
+				if match not in results:
+					results.append(match)
+		elif trackSearch:
+			if trackSearch.lower() in track.lower():
+				match = match = f.resultFormat(talk_dt, dt, x)
+				if match not in results:
+					results.append(match)
+		elif next:
+			if talk_d == d_struct:
+				if talk_t > t_struct:
+					match = '%s%s\n%s - %s%s\n' % (
+						co.BO, talk, start, room, co.E)
+					results.append(match)
+		else:
+			print('Use -t $string to search for a talk')
+			print('Use -n|--next to see remaining talks for today')
+			exit(0)
+	for entry in results:
+		print(entry)
+	if len(results) == 0:
+		print('%sNo results found%s' % (co.RE, co.E))
+	print('%sCurrent Time: %s%s%s' % (co.BO, co.GR, t, co.E))
+
 
 if __name__ == '__main__':
-    __init__()
+	__init__()
