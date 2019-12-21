1678ef985a873388061fb24d86c6f7536193ad04
path-braenaru <path_braenaru@inbox.ru>
2019-12-21 01:48:32 +0000

    Updated schedule for 36C3, small fixes

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 fahr.py
 =======
 
-fahr.py is an offline Fahrplan checking script written in Python (2 and 3 compatible).  Using the fahrplan 
+fahr.py is an offline Fahrplan checking script written in Python (both 2 and 3 compatible).  Using the fahrplan 
 schedule JSON, the script will either search for a talk by string or simply show remaining 
-talks for the day.  Don't let network outages ruin schedule checks!
+talks for the day, with the option to show full descriptions.  Don't let network lag or outages ruin schedule checks!
 
 Usage: python fahr.py [-t/--talk $string] [-n] [--track $string] [--classify]
 
@@ -11,7 +11,7 @@ Usage: python fahr.py [-t/--talk $string] [-n] [--track $string] [--classify]
 + -n will show remaining talks for the current date  
 + --track will show all results for a given track  
 + add --classify to see the associated classifiers (track relevance by %) in the output for the talks
-
+++ As of 2019/12/21: JSON with classifiers may be available again, but currently will not populate !
 talks that have already passed are returned in red, talks yet to occur will return in green
 
 Deps
--- fahr.py
+++ fahr.py
@@ -1,13 +1,15 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 
 
 import json
 import argparse
 
+
 from time import localtime, strftime, strptime
 
 
 class co:
+	CY = '\033[96m'
 	GR = '\033[92m'
 	RE = '\033[91m'
 	BO = '\033[1m'
@@ -18,9 +20,10 @@ class process():
 
 	def __init__(self):
 		self.classifiers = {}
+		self.tracks = []
 
 	def load(self):
-		with open('schedule.json', 'r') as jsonfile:
+		with open('sched36.json', 'r') as jsonfile:
 			for entry in jsonfile.readlines():
 				main_data = json.loads(entry)
 			jsonfile.close()
@@ -36,20 +39,18 @@ class process():
 		return main_data
 
 	def extract(self, data):
-		rooms = [
-			'Saal Clarke',
-			'Saal Adams',
-			'Saal Borg',
-			'Saal Dijkstra']
+		rooms = ['Ada', 'Borg', 'Clarke', 'Dijkstra', 'Eliza']
 		procData = []
 		for x in data['schedule']['conference']['days']:
 			for room in rooms:
 				for i in x['rooms'][room]:
-					tup = (i['title'], i['room'], i['date'],
+					tup = (i['title'].encode('utf-8'), i['room'], i['date'],
 						i['start'], i['duration'], i['track'],
-						i['id'], i['description'])
+						i['id'], i['description'].encode('utf-8'))
 					if tup not in procData:
 						procData.append(tup)
+					if i['track'] not in self.tracks:
+						self.tracks.append(i['track'])
 		return procData
 
 
@@ -59,38 +60,28 @@ class process():
 			a = co.GR
 		else:
 			a = co.RE
-		match = '%s%s\n - Room: %s\n - Date: %s\n - ' % (
-				a, talk, room, date[0:10]) +\
-			'Track: %s\n - Start: %s\n - Duration: %s%s\n' % (
-				track, start, duration, co.E)
+		match = '{}{}\n - Room: {}\n - Date: {}\n - '.format(a, talk.decode('utf-8'), room, date[0:10]) +\
+			'Track: {}\n - Start: {}\n - Duration: {}{}\n'.format(track, start, duration, co.E)
 		if isDesc:
-			match += '%s - Description:\n%s\n%s' % (a, desc, co.E)
+			match += '{} - Description:\n{}\n{}'.format(a, desc.decode('utf-8'), co.E)
 		if classify:
 			heat = ' - Classifiers:\n'
 			if id in self.classifiers:
-				for k, entry in sorted(self.classifiers[id].iteritems(),
-					key=lambda (a,b):(b,a), reverse=True):
-						heat += '\t- %s: %s\n' % (
-							k, entry)
-			match += '%s%s%s' % (a, heat, co.E)
+				for k, entry in sorted(self.classifiers[id].items(),
+					key=lambda a: a[1], reverse=True):
+						heat += '\t- {}: {}\n'.format(k, entry)
+			match += '{}{}{}'.format(a, heat, co.E)
 
 		return match
 
 
 def __init__():
 	p = argparse.ArgumentParser()
-	p.add_argument(
-		'-t', '--title'
-	)
-	p.add_argument(
-		'-n', '--next', action='store_true'
-	)
-	p.add_argument(
-		'--track')
-	p.add_argument(
-		'--classify', action='store_true')
-	p.add_argument(
-		'--description', action='store_true')
+	p.add_argument('-t', '--title', help='Search for a title by string')
+	p.add_argument('-n', '--next', action='store_true', help='Show remaining talks for the current day')
+	p.add_argument('--track', help='Search for all talks in a track (USE FIRST TRACK WORD ONLY)')
+	p.add_argument('--classify', action='store_true', help='Show track relevance by classifiers')
+	p.add_argument('--description', action='store_true', help='Append talk descriptions to output items')
 	args = p.parse_args()
 	classify = args.classify
 	title = args.title
@@ -104,7 +95,7 @@ def __init__():
 	d_struct = strptime(d, '%Y-%m-%d')
 	t = strftime('%H:%M', localtime())
 	t_struct = strptime(t, '%H:%M')
-	results = []
+	results = {'Ada': [], 'Borg': [], 'Clarke': [], 'Dijkstra': [], 'Eliza': []}
 	procData = f.extract(data)
 	for x in procData:
 		(talk, room, date, start, duration, track, id, desc) = x
@@ -113,30 +104,29 @@ def __init__():
 		talk_d = strptime(date[0:10], '%Y-%m-%d')
 		talk_t = strptime(start, '%H:%M')
 		if title:
-			if title.lower() in talk.lower():
+			if title.lower() in str(talk).lower():
 				match = f.resultFormat(talk_dt, dt, x, classify, isDesc)
-				if match not in results:
-					results.append(match)
+				results[room].append(match)
 		elif trackSearch:
 			if trackSearch.lower() in track.lower():
 				match = f.resultFormat(talk_dt, dt, x, classify, isDesc)
 				if match not in results:
-					results.append(match)
+					results[room].append(match)
 		elif next:
 			if talk_d == d_struct:
 				if talk_t > t_struct:
-					match = '%s%s\n%s - %s%s\n' % (
-						co.BO, talk, start, room, co.E)
-					results.append(match)
+					match = '{}{}\nStart: {} Length: {}{}\n\n'.format(
+						co.BO, talk.decode('utf-8'), start, duration, co.E)
+					results[room].append(match)
 		else:
 			print('Use -t $string to search for a talk')
 			print('Use -n|--next to see remaining talks for today')
 			exit(0)
-	for entry in results:
-		print(entry)
+	for entry in sorted(results):
+		print(str('{}Saal {}:{}\n\n{}'.format(co.CY, entry, co.E, '\n'.join(results[entry]))))
 	if len(results) == 0:
-		print('%sNo results found%s' % (co.RE, co.E))
-	print('%sCurrent Time: %s%s%s' % (co.BO, co.GR, t, co.E))
+		print('{}No results found{}'.format(co.RE, co.E))
+	print('{}{}Tracks (use first word only!):\n{}\n\nCurrent Time: {}{} ({}){}'.format(co.E, co.BO, ', '.join(f.tracks),co.GR, t, d, co.E))
 
 
 if __name__ == '__main__':
